96a8a97208cfb4a76ced03a6338f49c7aa565459
Santiago Leira <santiago.leira@wildlifestudios.com>
2020-07-12 17:29:15 -0300

    Generate hello world folder

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"github.com/reactivex/rxgo/v2"
+)
+
+func main() {
+	// By default, an Observable is lazy in the sense that it
+	// emits items only once a subscription is made.
+	observable := rxgo.Just("Hello, World!")()
+
+	<-observable.ForEach(
+		func(v interface{}) {
+			fmt.Printf("received: %v\n", v)
+		},
+		func(err error) {
+			fmt.Printf("error: %e\n", err)
+		},
+		func() {
+			fmt.Println("observable is closed")
+		},
+	)
+	//ch := observable.Observe()
+	//
+	//item := <-ch
+	//if item.Error() {
+	//	fmt.Println(item.E)
+	//	return
+	//}
+	//
+	//fmt.Print(item.V)
+}
